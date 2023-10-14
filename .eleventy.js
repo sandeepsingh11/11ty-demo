e22689dc12d964d0681fa281b76d81c53128f754
Sandeep Singh <serperior2@protonmail.com>
2023-10-13 22:04:21 -0400

    add tailwind, more updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles/main.css");
-};  
+    
+    // Input Directory : src Output directory: _site
+    return {
+        dir: { input: 'src', output: '_site' }
+    };
+};
